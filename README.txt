c3ee14f2fc90c155bbe7c4ecbc5c1afd30261040
Warwick Booth <warwick@lsdev.biz>
2021-01-14 16:42:04 +0200

    Updating the version number and readme files

--- README.txt
+++ README.txt
@@ -3,9 +3,9 @@ Contributors: feedmymedia, lightspeedwp, eleshar, krugazul, jacquesvdh, ignusver
 Donate link: https://lsdev.biz/lsx/donate/
 Tags: lsx, tour operator, activity operator, activities, activity bookings
 Requires at least: 5.0
-Tested up to: 5.5.1
+Tested up to: 5.6
 Requires PHP: 7.0
-Stable tag: 1.2.1
+Stable tag: 1.1.2
 License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
 
--- to-activities.php
+++ to-activities.php
@@ -3,7 +3,7 @@
  * Plugin Name: Tour Operator Activities
  * Plugin URI:  https://www.lsdev.biz/product/tour-operator-activities/
  * Description: The Tour Operator Activities extension adds “Activities” as a post type, which can be featured as part of a tour, at a specific destination, at an accommodation, and so on.
- * Version:     1.1.1
+ * Version:     1.1.2
  * Author:      LightSpeed
  * Author URI:  https://www.lsdev.biz/
  * License:     GPL3+
@@ -20,7 +20,7 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'LSX_ACTIVITIES_PATH', plugin_dir_path( __FILE__ ) );
 define( 'LSX_ACTIVITIES_CORE', __FILE__ );
 define( 'LSX_ACTIVITIES_URL', plugin_dir_url( __FILE__ ) );
-define( 'LSX_ACTIVITIES_VER', '1.1.1' );
+define( 'LSX_ACTIVITIES_VER', '1.1.2' );
 
 /* ======================= Below is the Plugin Class init ========================= */
 
